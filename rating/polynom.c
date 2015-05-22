24cdbf5ca296b9ec58b09becdb89c235ef42669f
Pavel Kurochkin <pavel.kurochkin@gmail.com>
2015-05-23 01:18:39 +0300

    Polynom is calculated in the range -40..40
    Values which are out of range are saturated to this range.

--- rating/polynom.c
+++ rating/polynom.c
@@ -1,10 +1,20 @@
 #include <math.h>
 #include "polynom.h"
 
+#define X_MIN  -40.0
+#define X_MAX  40.0
+
 // see 'octave' manual (polyval and polyfit functions)
+// 
+// see also: polyval.octave and data tables bestof3, bestof5.
+// function calculates value in the range specified by that tables (X_MIN..X_MAX).
 //
+// if value is out of range then it is saturated.
 double polyval( const struct polynom* p, double x )
 {
+  if ( x < X_MIN ) x = X_MIN;
+  if ( x > X_MAX ) x = X_MAX;
+
   int i = 0;
   double result = 0;
   for ( i = POLYNOM_COEFF_CNT - 1; i >=0; i -- ) {
